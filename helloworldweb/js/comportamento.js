bdb00349e83fd2292af77dc54b63a9c7fed892e3
Jonas Bento <jonasbento@outlook.com>
2018-11-09 18:31:56 -0200

    Exercícios antigos adicionados.

---
+++ helloworldweb/js/comportamento.js
@@ -0,0 +1,7 @@
+function exibirDataHora() {
+    document.getElementById("datahora").innerHTML = Date();
+}
+/*window.onload = function(){
+    var btn = document.getElementById("btn");
+    btn.onclick = exibirDataHora;
+}*/
