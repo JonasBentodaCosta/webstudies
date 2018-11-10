6fe29c5f865c5a15bd2a48bae8556e53b20bdd62
Jonas Bento <jonasbento@outlook.com>
2018-11-10 18:16:50 -0200

    Arquivos removidos.

--- helloworldweb/js/comportamento.js
+++
@@ -1,7 +0,0 @@
-function exibirDataHora() {
-    document.getElementById("datahora").innerHTML = Date();
-}
-/*window.onload = function(){
-    var btn = document.getElementById("btn");
-    btn.onclick = exibirDataHora;
-}*/
